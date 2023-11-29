f6ae2f8ca070c7bd426f62c7de8f89afe4923708
陈健祥 <chenjx@citydo.com.cn>
2023-11-29 18:00:30 +0800

    init

--- vue-apollo-client/vite.config.js
+++ vue-apollo-client/vite.config.js
@@ -1,18 +1,26 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-  ],
+  plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  server: {
+    host: 'localhost',
+    port: 5173,
+    proxy: {
+      '/graphql': {
+        target: 'http://localhost:4000/',
+        changeOrigin: true,
+        ws: true,
+      },
+    },
+  },
+});
